0dc02c52dbdad1cdab7822609709afada3a2b313
Cole Mickens <cole.mickens@gmail.com>
2021-05-18 14:31:09 -0700

    now use busybox ash

--- pkgs/kata-kernel/default.nix
+++ pkgs/kata-kernel/default.nix
@@ -2,11 +2,13 @@
 , linuxPackages_latest
 , ... }:
 
-(linuxPackages_latest.extend (lib.const (ksuper: {
+let linuxPackages = linuxPackages_latest.extend (lib.const (ksuper: {
   kernel = ksuper.kernel.override {
     extraConfig = ''
       FUSE_FS y
       VIRTIO_FS y
     '';
   };
-}))).kernel
+}));
+in
+  linuxPackages.kernel
